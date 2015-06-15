e1a51a502461a82ff3efb85e0c7b9a084955f297
Guanqing <haoguanqing@live.com>
2015-06-15 17:28:26 -0400

    version 2.0 updated

---
+++ app/src/main/java/com/example/administrator/myappportfolio/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.administrator.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Toast myToast=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMsg(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch "+ ((Button)view).getText().toString();
+        if (myToast!=null){
+            myToast.cancel();
+        }
+        myToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+}
